e234db576cda05ddf32c50362368002d2604af90
RafaelaSoliva <rafaelasoliva@outlook.com>
2025-01-08 15:19:59 -0300

    Feat: Classe Veiculo

---
+++ Estacionamento.Console/Models/Veiculo.cs
@@ -0,0 +1,9 @@
+namespace Models;
+
+public class Veiculo {
+    public string placa { get; set; }
+    public string marca { get; set; }
+    public string cor { get; set; }
+    public DateTime entrada { get; set; }
+    public DateTime saida { get; set; }
+}

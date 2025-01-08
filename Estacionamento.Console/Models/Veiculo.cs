4580515cdd661d706cbbce9ef9dc3ed00901d54d
RafaelaSoliva <rafaelasoliva@outlook.com>
2025-01-08 16:00:55 -0300

    Docs: Atualização dos comentários

--- Estacionamento.Console/Models/Veiculo.cs
+++ Estacionamento.Console/Models/Veiculo.cs
@@ -15,10 +15,13 @@ public class Veiculo {
     }
 
     public decimal CalcularValorAReceber (decimal PrecoInicial, decimal PrecoPorHora) {
+        // Registra o horário atual como horário de saída
         Saida = DateTime.Now;
 
+         // Calcula a diferença de horas entre a entrada e a saída
         TimeSpan timeSpan = Saida - Entrada;
 
+        // Calcula o valor a receber com base no tempo estacionado
         decimal ValorAReceber = PrecoInicial + (PrecoPorHora * timeSpan.Hours);
 
         return ValorAReceber;

9856ae841b04baf62cac5f59d17f82abcf3f426c
RafaelaSoliva <rafaelasoliva@outlook.com>
2025-01-09 17:54:34 -0300

    Refactor: Modificadores de acesso

--- Estacionamento.Console/Models/Veiculo.cs
+++ Estacionamento.Console/Models/Veiculo.cs
@@ -2,10 +2,10 @@ namespace Models;
 
 public class Veiculo {
     public string Placa { get; set; }
-    public string Marca { get; set; }
-    public string Cor { get; set; }
-    public DateTime Entrada { get; set; }
-    public DateTime Saida { get; set; }
+    private string Marca { get; set; }
+    private string Cor { get; set; }
+    private DateTime Entrada { get; set; }
+    private DateTime Saida { get; set; }
 
     public Veiculo (string placa, string marca, string cor) {
         Placa = placa;

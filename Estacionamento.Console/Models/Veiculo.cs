1e3d7e4ea7d4a7f538abad2af82b22a95a7239dd
RafaelaSoliva <rafaelasoliva@outlook.com>
2025-06-04 19:14:01 -0300

    Add: Classe Veiculo

---
+++ Estacionamento.Console/Models/Veiculo.cs
@@ -0,0 +1,20 @@
+namespace Estacionamento.Models;
+
+public class Veiculo {
+    public int Tipo { get; set; }
+    public string Cor { get; set; }
+    public string Placa { get; set; }
+    public DateTime Entrada { get; set; }
+
+    public Veiculo (int tipo, string cor, string placa) {
+        Tipo = tipo;
+        Cor = cor;
+        Placa = placa;
+        Entrada = DateTime.Now;
+    }
+
+    public int ObterHorasEstacionadas () {
+        TimeSpan ts = DateTime.Now - Entrada;
+        return ts.Hours;
+    }
+}
